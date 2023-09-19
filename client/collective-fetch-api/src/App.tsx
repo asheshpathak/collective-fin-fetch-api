ee0f719f1ceba9dcbf9a9fa28a44a04344619fe1
Ashesh Pathak <ashpatha@tibco.com>
2023-09-19 15:55:36 +0530

    corrected cookie import

--- client/collective-fetch-api/src/App.tsx
+++ client/collective-fetch-api/src/App.tsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./Components/Login";
 import { Homepage } from "./Components/Homepage";
-import Cookies from "react-cookie";
+import * as ReactCookies from "react-cookie";
 
 function App() {
   // const [accessToken, setAccessToken] = useState<String>("");
   // const headers = new Headers();
   // const access_token: string | null = headers.get("x-access-token");
   // setAccessToken(access_token);
-  console.log(Cookies);
+  console.log(ReactCookies.useCookies);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={access_token ? <Homepage /> : <Login />} />
+            <Route index element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Homepage />} />
           </Route>
