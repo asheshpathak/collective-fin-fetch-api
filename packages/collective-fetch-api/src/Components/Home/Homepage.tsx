64743cd359952933c5bd400acae509fece49aeae
Ashesh Pathak <ashesh.cosmid@gmail.com>
2023-11-27 20:21:05 +0530

    added prettier

--- packages/collective-fetch-api/src/Components/Home/Homepage.tsx
+++ packages/collective-fetch-api/src/Components/Home/Homepage.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { GetQuotes } from "./GetQuotes";
+import { GetQuotes } from "../GetQuotes";
 
 export const Homepage = () => {
   return (
     <div>
       <h1>Hello from HomePage</h1>
-      <GetQuotes />
     </div>
   );
 };
