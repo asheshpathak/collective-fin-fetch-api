3eb7d252bf1b450003bf528b42292b97f261ac08
Ashesh Pathak <ashesh.cosmid@gmail.com>
2023-12-17 19:17:15 +0530

    code cleanup and added router

--- packages/collective-fetch-api/src/App.tsx
+++ packages/collective-fetch-api/src/App.tsx
@@ -4,6 +4,10 @@ import { Login } from "./Components/Login";
 import { Homepage } from "./Components/Home/Homepage";
 import axios from "axios";
 import { GetQuotes } from "./Components/GetQuotes";
+import { Expenses } from "./Components/Expenses/Expenses";
+import { Overview } from "./Components/Overview/Overview";
+import { Portfolio } from "./Components/Portfolio/Portfolio";
+import { Positions } from "./Components/Positions/Positions";
 
 function App() {
   const [accessToken, setAccessToken] = useState<string>("sample access token");
@@ -17,15 +21,18 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/">
+          <Route path="/" element={<Homepage/>}>
             {/* <Route
               index
               element={accessToken?.length ? <Homepage /> : <GetQuotes />}
             /> */}
-            <Route index element={<Homepage />}
+            <Route index element={<Overview />}
             />
             <Route path="/login" element={<Login />} />
             {/* <Route path="/home" element={<Homepage />} /> */}
+            <Route path="/portfolio" element= {<Portfolio/>} />
+            <Route path="/positions" element= {<Positions/>} />
+            <Route path="/expenses" element= {<Expenses/>} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ packages/collective-fetch-api/src/Components/Overview/Overview.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Overview = () => {
+
+    return (
+        <div>
+            <h1>Hello from Overview!</h1>
+        </div>
+    )
+}
